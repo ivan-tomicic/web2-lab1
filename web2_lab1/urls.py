0b4594b6da0396d26421f3da9b3842992f3ba96e
Ivan Tomicic <ivan.tomicic@fer.hr>
2022-10-23 05:44:45 +0200

    add static to settings

--- web2_lab1/urls.py
+++ web2_lab1/urls.py
@@ -18,9 +18,11 @@ from django.urls import path
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('league/', include('league.urls')),
     path('admin/', admin.site.urls),
     path('', include('auth_app.urls')),
+    path('', RedirectView.as_view(url='/league/')),
 ]
