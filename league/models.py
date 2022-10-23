2b787fb4a06930478d99f7761fba4c3d30fffcec
Ivan Tomicic <ivan.tomicic@fer.hr>
2022-10-23 03:54:45 +0200

    remove unnecessary models

--- league/models.py
+++ league/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 class User(models.Model):
     email = models.EmailField(null=False, blank=False, unique=True)
@@ -15,28 +15,17 @@ class Team(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     home_stadium = models.ForeignKey(Stadium, unique=False, null=True, on_delete=models.SET_NULL)
 
-
-class Season(models.Model):
-    begin_year = models.IntegerField(null=False)
-    end_year = models.IntegerField(null=False)
-    number_of_rounds = models.IntegerField(null=False)
-    teams = models.ManyToManyField(Team)
-
-
-class MatchRound(models.Model):
-    number = models.IntegerField(null=False)
-    season = models.ForeignKey(Season, null=False, on_delete=models.PROTECT)
-
 class Match(models.Model):
     home_team = models.ForeignKey(Team, unique=False, null=False, on_delete=models.PROTECT, related_name="home_team")
     away_team = models.ForeignKey(Team, unique=False, null=False, on_delete=models.PROTECT, related_name="away_team")
     begin_time = models.DateTimeField(null=False)
     end_time = models.DateTimeField(null=False)
     stadium = models.ForeignKey(Stadium, null=False, unique=False, on_delete=models.PROTECT)
+    match_round = models.IntegerField(null=False, validators=[MinValueValidator(1), MaxValueValidator(36)])
 
 class Comment(models.Model):
     text = models.TextField(blank=False, max_length=10000, unique=False)
     user = models.ForeignKey(User, unique=False, null=False, on_delete=models.CASCADE)
-    match_round = models.ForeignKey(MatchRound, unique=False, null=True, on_delete=models.CASCADE)
+    match_round = models.IntegerField(null=False, validators=[MinValueValidator(1), MaxValueValidator(36)])
 
 
