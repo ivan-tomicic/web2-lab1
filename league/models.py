59dcdb85389d43ee76651d8ced3a3432f43e8c69
Ivan Tomicic <ivan.tomicic@fer.hr>
2022-10-25 01:31:43 +0200

    add comment component, change models

--- league/models.py
+++ league/models.py
@@ -1,20 +1,9 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
 
-class User(models.Model):
-    email = models.EmailField(null=False, blank=False, unique=True)
-    avatar = models.URLField(null=True, blank=False, unique=True)
-    first_name = models.CharField(max_length=50, blank=False, null=False)
-    last_name = models.CharField(max_length=50, blank=False, null=False)
-
-class Stadium(models.Model):
-    name = models.CharField(max_length=100, null=False, blank=False)
-    capacity = models.IntegerField(null=True)
-
 class Team(models.Model):
     name = models.CharField(max_length=100, blank=False, null=False)
     photo_url = models.URLField(null=True)
-    home_stadium = models.ForeignKey(Stadium, unique=False, null=True, on_delete=models.SET_NULL)
 
 class Match(models.Model):
     home_team = models.ForeignKey(Team, unique=False, null=False, on_delete=models.PROTECT, related_name="home_team")
@@ -23,18 +12,19 @@ class Match(models.Model):
     away_team_goals = models.IntegerField(null=True)
     begin_time = models.DateTimeField(null=True)
     end_time = models.DateTimeField(null=True)
-    stadium = models.ForeignKey(Stadium, null=False, unique=False, on_delete=models.PROTECT)
     match_round = models.IntegerField(null=False, validators=[MinValueValidator(1), MaxValueValidator(36)])
     game_played = models.BooleanField(null=False, default=False)
 
 class Comment(models.Model):
     text = models.TextField(blank=False, max_length=10000, unique=False)
-    user = models.ForeignKey(User, unique=False, null=False, on_delete=models.CASCADE)
+    username = models.CharField(max_length=200, null=False)
+    user_id = models.CharField(max_length=200, null=False)
     created_on = models.DateTimeField(auto_now_add=True)
     match_round = models.IntegerField(null=False, validators=[MinValueValidator(1), MaxValueValidator(36)])
 
 
 class TableTeam(models.Model):
+    photo_url = models.URLField()
     name = models.TextField(primary_key=True)
     matches_played = models.IntegerField()
     win = models.IntegerField()
--- league/views.py
+++ league/views.py
@@ -4,13 +4,12 @@ from django.db.models import Count, Case, When, Q, F
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from league.models import TableTeam, Match
+from league.models import TableTeam, Match, Comment
 
 logger = logging.getLogger('info')
 
 
 def index(request):
-    logger.info(request.session.get("user"))
     table_teams = [table_team for table_team in TableTeam.objects.order_by("-points", "-goal_difference", "-goals_for").values()]
     match_rounds = [r for r in Match.objects.values("match_round").annotate(played=Count(Case(When(game_played=True, then=1))),
                                                                      not_played=Count(Case(When(game_played=False, then=1)))).order_by("match_round")]
@@ -27,22 +26,21 @@ def index(request):
     )
 
 def match_round(request, match_round):
-    logger.info('round')
-    logger.info(match_round)
 
-    matches = [m for m in Match.objects.filter(match_round=match_round).order_by("game_played").annotate(home_team_name=F('home_team__name'),
+    matches = [m for m in Match.objects.filter(match_round=match_round).order_by("-game_played").annotate(home_team_name=F('home_team__name'),
                                                                                                          away_team_name=F('away_team__name'),
                                                                                                          home_team_photo=F('home_team__photo_url'),
                                                                                                          away_team_photo=F('away_team__photo_url')).values()]
-    print(matches)
+
+    comments = Comment.objects.filter(match_round=match_round).order_by("-created_on")
     return render(
         request,
         "match_round.html",
         context={
             "session": request.session.get("user"),
-            "pretty": json.dumps(request.session.get("user"), indent=4),
             "round_number": match_round,
-            "matches": matches
+            "matches": matches,
+            "comments": comments if request.session.get("user") else None
         },
     )
 
