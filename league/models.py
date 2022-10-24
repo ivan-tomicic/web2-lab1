60af3700db108803fdca88553c4cf2248ea0321d
Ivan Tomicic <ivan.tomicic@fer.hr>
2022-10-24 23:52:55 +0200

    add match rounds

--- league/models.py
+++ league/models.py
@@ -18,12 +18,13 @@ class Team(models.Model):
 class Match(models.Model):
     home_team = models.ForeignKey(Team, unique=False, null=False, on_delete=models.PROTECT, related_name="home_team")
     away_team = models.ForeignKey(Team, unique=False, null=False, on_delete=models.PROTECT, related_name="away_team")
-    home_team_goals = models.IntegerField(null=False)
-    away_team_goals = models.IntegerField(null=False)
-    begin_time = models.DateTimeField(null=False)
-    end_time = models.DateTimeField(null=False)
+    home_team_goals = models.IntegerField(null=True)
+    away_team_goals = models.IntegerField(null=True)
+    begin_time = models.DateTimeField(null=True)
+    end_time = models.DateTimeField(null=True)
     stadium = models.ForeignKey(Stadium, null=False, unique=False, on_delete=models.PROTECT)
     match_round = models.IntegerField(null=False, validators=[MinValueValidator(1), MaxValueValidator(36)])
+    game_played = models.BooleanField(null=False, default=False)
 
 class Comment(models.Model):
     text = models.TextField(blank=False, max_length=10000, unique=False)
--- league/views.py
+++ league/views.py
@@ -1,9 +1,10 @@
 import json, logging
 
+from django.db.models import Count, Case, When
 from django.http import HttpResponse
 from django.shortcuts import render
 
-from league.models import TableTeam
+from league.models import TableTeam, Match
 
 logger = logging.getLogger('info')
 
@@ -11,6 +12,9 @@ logger = logging.getLogger('info')
 def index(request):
     logger.info(request.session.get("user"))
     table_teams = [table_team for table_team in TableTeam.objects.order_by("-points", "-goal_difference", "-goals_for").values()]
+    match_rounds = [round for round in Match.objects.values("match_round").annotate(played=Count(Case(When(game_played=True, then=1))),
+                                                                     not_played=Count(Case(When(game_played=False, then=1)))).order_by("match_round")]
+    print(match_rounds)
 
     return render(
         request,
@@ -18,9 +22,15 @@ def index(request):
         context={
             "session": request.session.get("user"),
             "pretty": json.dumps(request.session.get("user"), indent=4),
-            "teams": table_teams
+            "teams": table_teams,
+            "match_rounds": match_rounds
         },
     )
+
+def match_round(request, match_round):
+    logger.info('round')
+    logger.info(match_round)
+
 
 
 
