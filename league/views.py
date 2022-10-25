5a5b874729d76f5ed6ca9c5d44ca321d53b14470
Ivan Tomicic <ivan.tomicic@fer.hr>
2022-10-25 16:23:28 +0200

    add delete button

--- league/views.py
+++ league/views.py
@@ -29,12 +29,14 @@ def index(request):
         },
     )
 
+@api_view(['GET'])
 def match_round(request, match_round):
 
     matches = [m for m in Match.objects.filter(match_round=match_round).order_by("-game_played").annotate(home_team_name=F('home_team__name'),
                                                                                                          away_team_name=F('away_team__name'),
                                                                                                          home_team_photo=F('home_team__photo_url'),
                                                                                                          away_team_photo=F('away_team__photo_url')).values()]
+
 
     comments = Comment.objects.filter(match_round=match_round).order_by("-created_on")
     return render(
@@ -44,7 +46,7 @@ def match_round(request, match_round):
             "session": request.session.get("user"),
             "round_number": match_round,
             "matches": matches,
-            "comments": comments if request.session.get("user") else None
+            "comments": comments if request.session.get("user") else None,
         },
     )
 
@@ -56,8 +58,8 @@ def comment(request):
     user_id = request.data['user_id']
     username = request.data['username']
     match_round = request.data['match_round']
-    Comment.objects.create(match_round=match_round, user_id=user_id, username=username,text=comment_text)
-    return redirect('match_round', match_round=match_round)
+    Comment.objects.create(match_round=match_round, user_id=user_id, username=username, text=comment_text)
+    return HttpResponse(status=200)
 
 
 
